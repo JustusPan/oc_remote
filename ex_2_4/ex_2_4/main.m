92dd8323d7a9ea7f5ee397061c8c219f4d78ece1
JustusPan <jpanchn@gmail.com>
2016-04-03 15:59:56 +0800

    exercise 2-4

---
+++ ex_2_4/ex_2_4/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  ex_2_4
+//
+//  Created by PanJingtian on 4/3/16.
+//  Copyright © 2016 ulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int value1, value2, sum;
+        value1 = 87;
+        value2 = 15;
+        sum = value1 - value2;
+        NSLog(@"%i - %i = %i", value1, value2, sum);
+    }
+    return 0;
+}
