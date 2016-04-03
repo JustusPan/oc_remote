1e9c551610be2514fbaf61468bdffe746a5f0c1c
JustusPan <jpanchn@gmail.com>
2016-04-03 15:57:34 +0800

    exercise 2-3

---
+++ ex_2_3/ex_2_3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ex_2_3
+//
+//  Created by PanJingtian on 4/3/16.
+//  Copyright © 2016 ulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int i;
+        i = 1;
+        NSLog(@"Testing...");
+        NSLog(@"....%i", i);
+        NSLog(@"...%i", i+1);
+        NSLog(@"..%i", i+2);
+    }
+    return 0;
+}
