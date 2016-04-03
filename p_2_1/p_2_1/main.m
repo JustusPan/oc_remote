b6c928aa2c092977f5d631115468a04d7ae71c2a
JustusPan <jpanchn@gmail.com>
2016-04-03 10:57:38 +0800

    program 2-1

---
+++ p_2_1/p_2_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  p_2_1
+//
+//  Created by PanJingtian on 4/3/16.
+//  Copyright © 2016 ulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
