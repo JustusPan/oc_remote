095c77ed00ac1216bc4151fd67e95cb5412b2243
JustusPan <jpanchn@gmail.com>
2016-04-03 16:05:54 +0800

    exercise 2-6

---
+++ ex_2_6/ex_2_6/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ex_2_6
+//
+//  Created by PanJingtian on 4/3/16.
+//  Copyright © 2016 ulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int answer, result;
+        
+        answer = 100;
+        result = answer - 10;
+        
+        NSLog(@"The result is %i \n", result + 5);
+    }
+    return 0;
+}
