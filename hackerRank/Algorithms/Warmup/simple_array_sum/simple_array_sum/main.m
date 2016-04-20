f6dd480bedbc0a7d22b1d649a34f2fcb47352a40
JustusPan <jpanchn@gmail.com>
2016-04-20 22:13:48 +0800

    a_very_big_sum first commit

--- hackerRank/Algorithms/Warmup/simple_array_sum/simple_array_sum/main.m
+++ hackerRank/Algorithms/Warmup/simple_array_sum/simple_array_sum/main.m
@@ -8,10 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+int sum(int *arr, int n) {
+    int arrSum = 0;
+    for (int arr_i = 0;arr_i<n;++arr_i) {
+        arrSum += arr[arr_i];
+    }
+    return arrSum;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int n;
+        scanf("%i",&n);
+        int arr[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            scanf("%d",&arr[arr_i]);
+        }
+        int res = sum(arr,n);
+        printf("%d",res);
     }
     return 0;
 }
