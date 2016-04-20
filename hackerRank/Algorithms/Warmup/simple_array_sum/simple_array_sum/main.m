b2ff17696819ec7079d1a2c87717331eb2ba9faa
JustusPan <jpanchn@gmail.com>
2016-04-20 21:53:25 +0800

    solve_me_first second commit

---
+++ hackerRank/Algorithms/Warmup/simple_array_sum/simple_array_sum/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  simple_array_sum
+//
+//  Created by PanJingtian on 4/20/16.
+//  Copyright © 2016 ulab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
